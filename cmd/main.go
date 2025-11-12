2ba3de4b4733142ce3e388daa03f1631b621db7b
Vladimirmoscow84 <ccr1@yandex.ru>
2025-11-12 14:05:31 +0300

    begin progect

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Vladimirmoscow84/Image_processor/internal/app"
+
+func main() {
+	app.Run()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Vladimirmoscow84/Image_processor
+
+go 1.25.0
