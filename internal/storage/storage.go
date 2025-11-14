92242d926121623f76493fb6c06501d6b8f875a8
Vladimirmoscow84 <ccr1@yandex.ru>
2025-11-15 01:16:51 +0300

    fix Storage

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,23 +1,31 @@
 package storage
 
 import (
-	"errors"
+	"fmt"
 	"log"
 
+	filestorage "github.com/Vladimirmoscow84/Image_processor/internal/storage/file_storage"
 	"github.com/Vladimirmoscow84/Image_processor/internal/storage/postgres"
 )
 
 type Storage struct {
 	*postgres.Postgres
+	*filestorage.FileStorage
 }
 
 // New - конструктор storage
-func New(pg *postgres.Postgres) (*Storage, error) {
+func New(pg *postgres.Postgres, fs *filestorage.FileStorage) (*Storage, error) {
 	if pg == nil {
 		log.Println("[storage] postgres client is nil")
-		return nil, errors.New("[storage] postgres client is nil")
+		return nil, fmt.Errorf("[storage] postgres client is nil")
 	}
+	if fs == nil {
+		log.Println("[storage] fileStorage client is nil")
+		return nil, fmt.Errorf("[storage] fileStorage client is nil")
+	}
+
 	return &Storage{
-		Postgres: pg,
+		Postgres:    pg,
+		FileStorage: fs,
 	}, nil
 }
