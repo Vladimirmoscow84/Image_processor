474f3edec336504d4b8bb6b7108e5cfd240cc91f
Vladimirmoscow84 <ccr1@yandex.ru>
2025-11-15 01:02:52 +0300

    add save method in file_storage

--- internal/storage/file_storage/file_storage.go
+++ internal/storage/file_storage/file_storage.go
@@ -1,8 +1,12 @@
 package filestorage
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
+	"time"
 )
 
 type FileStorage struct {
@@ -19,4 +23,34 @@ func New(path string) (*FileStorage, error) {
 	if err != nil {
 		return nil, fmt.Errorf("[fileStorage] failed to create base dir: %w", err)
 	}
+	return &FileStorage{
+		Path: path,
+	}, nil
+}
+
+// Save сохраняет файл в хранилище и возвращает путь к нему
+func (f *FileStorage) Save(ctx context.Context, origPath string) (string, error) {
+
+	//создание уникального имени файла
+	fileName := fmt.Sprintf("%d_%s", time.Now().UnixNano(), filepath.Base(origPath))
+	destPath := filepath.Join(f.Path, fileName)
+
+	//копироание файла
+	in, err := os.Open(origPath)
+	if err != nil {
+		return "", fmt.Errorf("[fileStorage] failed to open file: %w", err)
+	}
+	defer in.Close()
+
+	out, err := os.Create(destPath)
+	if err != nil {
+		return "", fmt.Errorf("[fileStorage] failed to create file %w", err)
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return "", fmt.Errorf("[fileStorage] failed to copy file: %w", err)
+	}
+	return destPath, nil
 }
