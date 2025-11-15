066d9f8c883767bc2abffa8ee65dbf55160112be
Vladimirmoscow84 <ccr1@yandex.ru>
2025-11-16 00:06:01 +0300

    add kafka open close functions

--- internal/queue_broker/kafka/kafka.go
+++ internal/queue_broker/kafka/kafka.go
@@ -1,1 +1,47 @@
 package kafka
+
+import (
+	"log"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Config struct {
+	Brokers []string
+	Topic   string
+	GroupID string
+}
+
+// NewProducer созадет kafka producer
+func NewProducer(cfg Config) *kafka.Writer {
+	return kafka.NewWriter(kafka.WriterConfig{
+		Brokers:  cfg.Brokers,
+		Topic:    cfg.Topic,
+		Balancer: &kafka.LeastBytes{},
+	})
+}
+
+// NewConsumer создает kafka consumer
+func NewConsumer(cfg Config) *kafka.Reader {
+	return kafka.NewReader(kafka.ReaderConfig{
+		Brokers: cfg.Brokers,
+		GroupID: cfg.GroupID,
+		Topic:   cfg.Topic,
+	})
+}
+
+// CloseProducer закрывает kafka producer
+func CloseProducer(producer *kafka.Writer) {
+	err := producer.Close()
+	if err != nil {
+		log.Printf("[kafka] failed to close producer: %v", err)
+	}
+}
+
+// CloseConsumer закрывает kafka consumer
+func CloseConsumer(consumer *kafka.Reader) {
+	err := consumer.Close()
+	if err != nil {
+		log.Printf("[kafka] failrd tp clode consumer: %v", err)
+	}
+}
