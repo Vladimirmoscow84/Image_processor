a096ae1bc6d8e29263d5818885ff2659b5612c8a
Vladimirmoscow84 <ccr1@yandex.ru>
2025-11-17 14:56:04 +0300

    imagegetterhandler. done

--- internal/handlers/imageGetterHandler.go
+++ internal/handlers/imageGetterHandler.go
@@ -1,1 +1,24 @@
 package handlers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (r *Router) imageGetterHandler(c *gin.Context) {
+
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id parametr in comand line"})
+		return
+	}
+	image, err := r.imageGetter.GetImage(c.Request.Context(), id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, image)
+}
