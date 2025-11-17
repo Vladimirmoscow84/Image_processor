c5184222406d61e16a91c1f3659d5cd9bb507e31
Vladimirmoscow84 <ccr1@yandex.ru>
2025-11-17 15:02:47 +0300

    imageDeleterhandler done

--- internal/handlers/iamgeDeleterHandler.go
+++ internal/handlers/iamgeDeleterHandler.go
@@ -1,1 +1,28 @@
 package handlers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (r *Router) imageDeleterHandler(c *gin.Context) {
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id parametr in comand line"})
+	}
+	image, err := r.imageGetter.GetImage(c.Request.Context(), id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = r.imageDeleter.DeleteImage(c.Request.Context(), image)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"status": "deleted"})
+}
